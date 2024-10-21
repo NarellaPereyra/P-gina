5436cf7b81ce9e1e4f2da985b8964a62bd1a68a5
Narella Pereyra <npereyra5205@gmail.com>
2024-10-21 16:50:17 -0300

    M5

--- wc_node/models/novedadesModel.js
+++ wc_node/models/novedadesModel.js
@@ -1,9 +1,9 @@
 var pool = require('./bd');
 
 async function getNovedades() {
-        var query = 'select * from novedades order by id desc';
-        var rows = await pool.query(query);
-        return rows;
+    var query = 'select * from novedades order by id desc';
+    var rows = await pool.query(query);
+    return rows;
 }
 
 async function deleteNovedadesById(id) {
@@ -12,21 +12,21 @@ async function deleteNovedadesById(id) {
     return rows;
 }
 
-async function insertNovedad(obj){
-    try{
+async function insertNovedad(obj) {
+    try {
         var query = "insert into novedades set ?";
         var rows = await pool.query(query, [obj])
         return rows;
-    } catch (error){
-        console.log (error);
-        throw error; 
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
 
 async function getNovedadById(id) {
     var query = "select * from novedades where id = ?";
     var rows = await pool.query(query, [id]);
-    return rows [0];
+    return rows[0];
 }
 
 async function modificarNovedadById(obj, id) {
@@ -34,9 +34,15 @@ async function modificarNovedadById(obj, id) {
         var query = "update novedades set ? where id = ?";
         var rows = await pool.query(query, [obj, id]);
         return rows;
-    } catch (error){
+    } catch (error) {
         throw error;
     }
 }
 
-module.exports = { getNovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById}
+async function buscarNovedades(busqueda) {
+    var query = "select * from novedades where titulo like ? OR subtitulo like ? OR cuerpo like ?";
+    var rows = await pool.query(query, ['%' + busqueda + '%', '%' + busqueda + '%', '%' + busqueda + '%']);
+    return rows;
+}
+
+module.exports = { getNovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById, buscarNovedades }
